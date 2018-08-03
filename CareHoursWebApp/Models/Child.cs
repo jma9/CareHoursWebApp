dc66d6293eea4a1d417be884507373567f20053d
JMA <jani.m.manninen@gmail.com>
2018-08-03 12:51:04 +0300

    Add project files.

---
+++ CareHoursWebApp/Models/Child.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CareHoursWebApp.Models
+{
+    public class Child
+    {
+        public int ChildId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
